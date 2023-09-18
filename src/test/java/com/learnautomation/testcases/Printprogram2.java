fdb8186b13fb9fa08473c4be225921ba6735ced9
arun <akhatri@asite.com>
2023-09-18 15:44:27 +0530

    Added second programe for java file

--- src/test/java/com/learnautomation/testcases/Printprogram2.java
+++ src/test/java/com/learnautomation/testcases/Printprogram2.java
@@ -1,15 +1,15 @@
 package com.learnautomation.testcases;
 
-public class Printprogram {
+public class Printprogram2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int a=500;
+		int a=10;
 		int b=20;
 		int sum;
 		
-		sum=a*b;
+		sum=a+b;
 	System.out.println("Your sum is "+ sum);
 
 	}
