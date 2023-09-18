bf59140392ca6b813ddc57b50a9b7dd5940b7864
arun <akhatri@asite.com>
2023-09-18 15:19:00 +0530

    added some changes

---
+++ src/test/java/com/learnautomation/testcases/Printprogram.java
@@ -0,0 +1,17 @@
+package com.learnautomation.testcases;
+
+public class Printprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10;
+		int b=20;
+		int sum;
+		
+		sum=a+b;
+	System.out.println("Your sum is "+ sum);
+
+	}
+
+}
